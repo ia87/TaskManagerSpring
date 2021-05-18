be47fd16b7a099f335ab31d3c8dd00c7e67e2f3a
Andrii Cherniak <andrii_cherniak@epam.com>
2021-05-19 01:05:13 +0300

    Update RepositorySwitcher

--- src/main/java/com/my/taskmanagerspring/util/RepositorySwitcher.java
+++ src/main/java/com/my/taskmanagerspring/util/RepositorySwitcher.java
@@ -1,23 +1,35 @@
 package com.my.taskmanagerspring.util;
 
 import com.my.taskmanagerspring.interceptor.RequestData;
+import com.my.taskmanagerspring.repository.UserGeneralRepository;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 @Data
 public class RepositorySwitcher {
+    private final UserGeneralRepository JPARepo;
+    private final UserGeneralRepository JDBCRepo;
     @Autowired
     private RequestData requestData;
 
-    public String getRepository() {
+    public RepositorySwitcher(@Qualifier("JPARepo") UserGeneralRepository JPARepo,
+                              @Qualifier("JDBCRepo") UserGeneralRepository JDBCRepo) {
+        this.JPARepo = JPARepo;
+        this.JDBCRepo = JDBCRepo;
+    }
+
+
+    public UserGeneralRepository getRepository() {
         System.out.println(requestData);
         System.out.println(requestData.hashCode());
-        String repo = "JPA";
+//        String repo = "JPA";
+        UserGeneralRepository repo = JPARepo;
         while (requestData.getHeaderNames().hasMoreElements()) {
             if (requestData.getHeaderNames().nextElement().equals("jdbc")) {
-                repo = "JDBC";
+                repo = JDBCRepo;
             }
         }
         return repo;
